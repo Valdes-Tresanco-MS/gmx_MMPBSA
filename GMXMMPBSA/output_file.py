e642c2a128e9b50307164b6a9419d8d7d6d4a4cd
MARIO <mariosergiovaldes145@gmail.com>
2020-09-15 01:28:45 -0500

    GMX-MMGBSA-1 Update copyright

--- GMXMMPBSA/output_file.py
+++ GMXMMPBSA/output_file.py
@@ -1,26 +1,25 @@
 """
 This module is responsible for creating the final output file for MM/PBSA
 statistics printing.
-
-                           GPL LICENSE INFO
-
-Copyright (C) 2010  Dwight McGee, Billy Miller III, and Jason Swails
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.
 """
+
+# ##############################################################################
+#                           GPLv3 LICENSE INFO                                 #
+#                                                                              #
+#  Copyright (C) 2020  Mario S. Valdes-Tresanco and Mario E. Valdes-Tresanco   #
+#  Copyright (C) 2014  Jason Swails, Bill Miller III, and Dwight McGee         #
+#                                                                              #
+#   Project: https://github.com/Valdes-Tresanco-MS/GMX-MMGBSA                  #
+#                                                                              #
+#   This program is free software; you can redistribute it and/or modify it    #
+#  under the terms of the GNU General Public License version 3 as published    #
+#  by the Free Software Foundation.                                            #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful, but         #
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
+#  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    #
+#  for more details.                                                           #
+# ##############################################################################
 
 import io
 from GMXMMPBSA.amber_outputs import EnergyVector
--- GMXMMPBSA/parm_setup.py
+++ GMXMMPBSA/parm_setup.py
@@ -6,26 +6,26 @@ to translate complex atom selections into either Amber masks or Amber
 group input selection strings for the complex, receptor, and ligand.
 It will also check through the systems and make sure that the prmtop
 files are compatible, as well. Necessary for MMPBSA.py functioning.
-
-                          GPL LICENSE INFO                             
-
-  Copyright (C) 2009 - 2012 Jason Swails, Bill Miller III, and Dwight McGee
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-   
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330,
-  Boston, MA 02111-1307, USA.
 """
+
+# ##############################################################################
+#                           GPLv3 LICENSE INFO                                 #
+#                                                                              #
+#  Copyright (C) 2020  Mario S. Valdes-Tresanco and Mario E. Valdes-Tresanco   #
+#  Copyright (C) 2014  Jason Swails, Bill Miller III, and Dwight McGee         #
+#                                                                              #
+#   Project: https://github.com/Valdes-Tresanco-MS/GMX-MMGBSA                  #
+#                                                                              #
+#   This program is free software; you can redistribute it and/or modify it    #
+#  under the terms of the GNU General Public License version 3 as published    #
+#  by the Free Software Foundation.                                            #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful, but         #
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
+#  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    #
+#  for more details.                                                           #
+# ##############################################################################
+
 from parmed.amber import LoadParm
 from GMXMMPBSA.exceptions import PrmtopError, SelectionError
 
