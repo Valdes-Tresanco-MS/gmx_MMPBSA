ae513f060695c6b17da030ec5320bed7c236a631
MARIO <mariosergiovaldes145@gmail.com>
2020-10-10 09:03:09 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
 setup(
-    name='MMGBSA',
+    name='GMX-MMPBSA',
     version='1.0.0',
     packages=['GMXMMPBSA'],
-    url='',
     license='GPLv3',
     author='Mario S. Valdes-Trasanco and Mario E. Valdes-Tresanco ',
     author_email='mariosergiovaldes145@gmail.com',
-    description='Adaptation of MMPBSA.py (AMBER) to use Gromacs files'
+    maintainer='Mario S. Valdes-Trasanco',
+    maintainer_email='mariosergiovaldes145@gmail.com',
+    url='https://github.com/Valdes-Tresanco-MS/GMX-MMGBSA',
+    description='Adaptation of MMPBSA.py (AMBER) to use Gromacs files',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    keywords=['GMX-MMPBSA', 'MMPBSA', 'GROMACS', 'AmberTools'],
+    scripts=['GMX_MMPBSA.py']
 )
