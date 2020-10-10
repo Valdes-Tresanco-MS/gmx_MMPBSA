3e3bb7e39b22b0d236a5b46441dbbe829ed53135
MARIO <mariosergiovaldes145@gmail.com>
2020-10-10 15:42:37 -0500

    update setup

--- GMX_MMPBSA.py
+++ GMX_MMPBSA.py
@@ -42,65 +42,68 @@ except ImportError:
                       'you have sourced %s/amber.sh (if you are using sh/ksh/'
                       'bash/zsh) or %s/amber.csh (if you are using csh/tcsh)' %
                       (amberhome, amberhome))
+def run():
+    # Set up the MPI version or not based on whether our executable name ends in MPI
+    if sys.argv[0].endswith('MPI'):
+        try:
+            from mpi4py import MPI
+        except ImportError:
+            raise MMPBSA_Error('Could not import mpi4py package! Use serial version '
+                               'or install mpi4py.')
+    else:
+        # If we're not running MMPBSA.py.MPI, bring MPI into the top-level namespace
+        # (which will overwrite the MPI from mpi4py, which we *want* to do in serial)
+        from GMXMMPBSA.fake_mpi import MPI
 
-# Set up the MPI version or not based on whether our executable name ends in MPI
-if sys.argv[0].endswith('MPI'):
+    # Set up error/signal handlers
+    main.setup_run()
+
+    # Instantiate the main MMPBSA_App
+    app = main.MMPBSA_App(MPI)
+
+    # Read the command-line arguments
     try:
-        from mpi4py import MPI
-    except ImportError:
-        raise MMPBSA_Error('Could not import mpi4py package! Use serial version '
-                           'or install mpi4py.')
-else:
-    # If we're not running MMPBSA.py.MPI, bring MPI into the top-level namespace
-    # (which will overwrite the MPI from mpi4py, which we *want* to do in serial)
-    from GMXMMPBSA.fake_mpi import MPI
-
-# Set up error/signal handlers
-main.setup_run()
-
-# Instantiate the main MMPBSA_App
-app = main.MMPBSA_App(MPI)
-
-# Read the command-line arguments
-try:
-    app.get_cl_args(sys.argv[1:])
-except CommandlineError as e:
-    sys.stderr.write('%s: %s' % (type(e).__name__, e )+ '\n')
-    sys.exit(1)
-
-# Perform our MMPBSA --clean now
-if app.FILES.clean:
-    sys.stdout.write('Cleaning temporary files and quitting.\n')
-    app.remove(0)
-    sys.exit(0)
-
-# See if we wanted to print out our input file options
-if app.FILES.infilehelp:
-    app.input_file.print_contents(sys.stdout)
-    sys.exit(0)
-
-# If we're not rewriting output do whole shebang, otherwise load info and parms
-# Throw up a barrier before and after running the actual calcs
-if not app.FILES.rewrite_output:
-    try:
-        app.read_input_file()
-    except InputError as e:
+        app.get_cl_args(sys.argv[1:])
+    except CommandlineError as e:
         sys.stderr.write('%s: %s' % (type(e).__name__, e )+ '\n')
-        sys.stderr.write('  Enter `%s --help` for help\n' %
-                         (split(sys.argv[0])[1]))
         sys.exit(1)
-    app.process_input()
-    app.check_for_bad_input()
-    app.loadcheck_prmtops()
-    app.file_setup()
-    app.run_mmpbsa()
-# If we are rewriting output, load the info and check prmtops
-else:
-    info = InfoFile(app)
-    info.read_info()
-    app.loadcheck_prmtops()
 
-# Now we parse the output, print, and finish
-app.parse_output_files()
-app.write_final_outputs()
-app.finalize()
+    # Perform our MMPBSA --clean now
+    if app.FILES.clean:
+        sys.stdout.write('Cleaning temporary files and quitting.\n')
+        app.remove(0)
+        sys.exit(0)
+
+    # See if we wanted to print out our input file options
+    if app.FILES.infilehelp:
+        app.input_file.print_contents(sys.stdout)
+        sys.exit(0)
+
+    # If we're not rewriting output do whole shebang, otherwise load info and parms
+    # Throw up a barrier before and after running the actual calcs
+    if not app.FILES.rewrite_output:
+        try:
+            app.read_input_file()
+        except InputError as e:
+            sys.stderr.write('%s: %s' % (type(e).__name__, e )+ '\n')
+            sys.stderr.write('  Enter `%s --help` for help\n' %
+                             (split(sys.argv[0])[1]))
+            sys.exit(1)
+        app.process_input()
+        app.check_for_bad_input()
+        app.loadcheck_prmtops()
+        app.file_setup()
+        app.run_mmpbsa()
+    # If we are rewriting output, load the info and check prmtops
+    else:
+        info = InfoFile(app)
+        info.read_info()
+        app.loadcheck_prmtops()
+
+    # Now we parse the output, print, and finish
+    app.parse_output_files()
+    app.write_final_outputs()
+    app.finalize()
+
+if __name__ == '__main__':
+    run()
