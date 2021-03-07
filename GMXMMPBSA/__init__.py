9b0542bdddbd9dff39072cfb081e20b89c7977cf
MARIO <mariosergiovaldes145@gmail.com>
2021-03-07 03:21:18 -0500

    Added others authors

--- GMXMMPBSA/__init__.py
+++ GMXMMPBSA/__init__.py
@@ -25,7 +25,7 @@ __all__ = ['alamdcrd', 'amber_outputs', 'analyzer', 'API', 'app', 'calculation',
            'exceptions', 'fake_mpi', 'findprogs', 'infofile', 'input_parser', 'main', 'make_top', 'make_trajs',
            'output_file', 'parm_setup', 'timer', 'utils']
 
-__author__ = "Mario S. Valdes Tresanco and Mario E. Valdes Tresanco"
+__author__ = "Mario S. Valdes Tresanco, Mario E. Valdes Tresanco, Pedro A. Valiente PhD and Ernesto Moreno PhD"
 __license__ = "GPLv3"
 __mmpbsa_author__ = "Jason Swails, Dwight McGee, and Bill Miller III"
 __mmpbsa_version__ = "16.0"
