ce7ce0124239ad3694c57dad8f9136179c2e65d5
MARIO <mariosergiovaldes145@gmail.com>
2021-03-07 03:31:46 -0500

    fixed for temperature variable

--- GMXMMPBSA/calculation.py
+++ GMXMMPBSA/calculation.py
@@ -568,31 +568,31 @@ class InteractionEntropyCalc:
 
     def _calculate(self):
         # gases constant in kcal/mol
-        k = 0.001987
+        R = 0.001987
         energy_int = np.array([], dtype=np.float)
         a_energy_int = np.array([], dtype=np.float)
         d_energy_int = np.array([], dtype=np.float)
         exp_energy_int = np.array([], dtype=np.float)
         ts = np.array([], dtype=np.float)
-
         for eint in self.ggas:
             energy_int = np.append(energy_int, eint)
             aeint = energy_int.mean()
             a_energy_int = np.append(a_energy_int, aeint)
             deint = eint - aeint
             d_energy_int = np.append(d_energy_int, deint)
-            eceint = math.exp(deint / (k * self.INPUT['entropy_temp']))
+            eceint = math.exp(deint / (R * self.INPUT['temperature']))
             exp_energy_int = np.append(exp_energy_int, eceint)
             aeceint = exp_energy_int.mean()
-            cts = k * self.INPUT['entropy_temp'] * math.log(aeceint)
+            cts = R * self.INPUT['temperature'] * math.log(aeceint)
             ts = np.append(ts, cts)
         self.data = ts
         # self.data['IE_' + self.key] = self.IntEnt
         nframes = len(self.data)
         self.ie_frames = math.ceil(nframes * (self.INPUT['entropy_seg'] / 100))
         self.value = self.data[self.ie_frames:].mean()
-        self.frames = [x for x in range(self.INPUT['startframe'], self.INPUT['endframe'] + self.INPUT['interval'],
-                                       self.INPUT['interval'])]
+        self.frames = [x for x in range(self.INPUT['startframe'],
+                                        self.INPUT['startframe'] + nframes,
+                                        self.INPUT['interval'])]
 
     def save_output(self):
         with open(self.output, 'w') as out:
