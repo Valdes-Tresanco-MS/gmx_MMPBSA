3e14d6a75491d86fa23ae7118b03afc4ae0a10e4
Mario <mariosergiovaldes145@gmail.com>
2021-10-06 00:48:48 -0500

    implemented replot and redraw

--- GMXMMPBSA/analyzer/customitem.py
+++ GMXMMPBSA/analyzer/customitem.py
@@ -317,20 +317,30 @@ class CustomItem(QTreeWidgetItem):
         from GMXMMPBSA.analyzer.plots import LineChart
         self.app.treeWidget.clearSelection()
 
-        options = {'general_options': self.app.systems[self.system_index]['chart_options']['general_options'],
-                   'line_options': self.app.systems[self.system_index]['chart_options']['line_options'],
-                   'chart_title': self.chart_title, 'chart_subtitle': self.chart_subtitle}
+        options = self.app.systems[self.system_index]['chart_options'].get_settings()
+        options.update({'chart_title': self.chart_title, 'chart_subtitle': self.chart_subtitle})
+        changes = self.app.systems[self.system_index]['chart_options'].changes
 
         if state:
             self.setSelected(True)
-            if not self.lp_subw or self.frange != self.lp_subw.frange or self.line_change:
+            line_change = (changes['line_action'] == 3 or changes['line_ie_action'] == 3)
+
+            if not self.lp_subw or self.frange != self.lp_subw.frange or line_change:
                 self.lp_subw = LineChart(self.line_plot_data, self.line_chart_action, data2=self.ie_plot_data,
                                          options=options)
-                # darkgrid, whitegrid, dark, white, ticks
                 self.lp_subw.frange = self.frange  # set the frange
-                self.line_change = False  # make False again
+                # self.line_change = False  # make False again
+                changes['line_action'] = 0
+                changes['line_ie_action'] = 0
                 self.app.mdi.addSubWindow(self.lp_subw)
-            self.lp_subw.show()
+                self.lp_subw.show()
+            else:
+                line_change = (changes['line_action'] == 1 or changes['line_ie_action'] == 1)
+                if line_change:
+                    self.lp_subw.show()
+                    self.lp_subw.update_config(options)
+                    changes['line_action'] = 0
+                    changes['line_ie_action'] = 0
         elif self.lp_subw:
             self.app.mdi.activatePreviousSubWindow()
             self.lp_subw.close()
@@ -338,9 +348,10 @@ class CustomItem(QTreeWidgetItem):
     def plotting_bar(self, state):
         from GMXMMPBSA.analyzer.plots import BarChart
         self.app.treeWidget.clearSelection()
-        options = {'general_options': self.app.systems[self.system_index]['chart_options']['general_options'],
-                   'bar_options': self.app.systems[self.system_index]['chart_options']['bar_options'],
-                   'chart_title': self.chart_title, 'chart_subtitle': self.chart_subtitle}
+        options = self.app.systems[self.system_index]['chart_options'].get_settings()
+        options.update({'chart_title': self.chart_title, 'chart_subtitle': self.chart_subtitle})
+        changes = self.app.systems[self.system_index]['chart_options'].changes
+
         if 'groups' in self.properties:
             options['groups'] = self.properties['groups']
         if 'scalable' in self.properties:
@@ -348,12 +359,17 @@ class CustomItem(QTreeWidgetItem):
 
         if state:
             self.setSelected(True)
-            if not self.bp_subw or self.frange != self.bp_subw.frange or self.bar_change:
+            if not self.bp_subw or self.frange != self.bp_subw.frange or changes['bar_action'] == 3:
                 self.bp_subw = BarChart(self.bar_plot_data, self.bar_chart_action, options=options)
                 self.bp_subw.frange = self.frange
-                self.bar_change = False # make False again
+                # self.bar_change = False  # make False again
+                changes['bar_action'] = 0
                 self.app.mdi.addSubWindow(self.bp_subw)
-            self.bp_subw.show()
+                self.bp_subw.show()
+            elif changes['bar_action'] == 1:
+                self.bp_subw.show()
+                self.bp_subw.update_config(options)
+                changes['bar_action'] = 0
         elif self.bp_subw:
             self.app.mdi.activatePreviousSubWindow()
             self.bp_subw.close()
