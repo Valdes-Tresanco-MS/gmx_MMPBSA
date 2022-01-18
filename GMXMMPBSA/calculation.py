563b40eee73d75a5daa74091a85403aad23446da
Mario <mariosergiovaldes145@gmail.com>
2022-01-18 04:07:40 -0500

    added error when the internal energy have a large fluctuation

--- GMXMMPBSA/calculation.py
+++ GMXMMPBSA/calculation.py
@@ -645,6 +645,10 @@ class InteractionEntropyCalc:
             aeint = energy_int.mean()
             a_energy_int = np.append(a_energy_int, aeint)
             deint = eint - aeint
+            if deint > 425:
+                GMXMMPBSA_ERROR('The internal energy of your system has very large energy fluctuation so it is not '
+                                'possible to continue with the calculations. Please, make sure your system is '
+                                'consistent')
             eceint = math.exp(deint / (k * temp))
             exp_energy_int = np.append(exp_energy_int, eceint)
             aeceint = exp_energy_int.mean()
