8ee696cb0104138e27a4226d08f7e2c1657284f8
Mario <mariosergiovaldes145@gmail.com>
2022-01-21 03:22:08 -0500

    nogui options is set by default when --rewrite-output is defined

--- GMXMMPBSA/main.py
+++ GMXMMPBSA/main.py
@@ -651,7 +651,7 @@ class MMPBSA_App(object):
         self.MPI.Finalize()
 
         end = 0
-        if self.FILES.gui:
+        if self.FILES.gui and not self.FILES.rewrite_output:
             import subprocess
             logging.info('Opening gmx_MMPBSA_ana to analyze results...\n')
             g = subprocess.Popen(['gmx_MMPBSA_ana', '-f', self.FILES.prefix + 'info'])
