fixed IE/C2 plots and rolling average

GMXMMPBSA/analyzer/plots.py
@@ -176,10 +176,10 @@ class LineChart(ChartsBase):
         self.fig.set_size_inches(options[('Line Plot', 'figure', 'width')],
                                  options[('Line Plot', 'figure', 'height')])
 
-        width1 = 100 - options[('Line Plot', 'Interaction Entropy', 'bar-plot', 'width')]
-        width2 = options[('Line Plot', 'Interaction Entropy', 'bar-plot', 'width')]
-        height1 = 100 - options[('Line Plot', 'Interaction Entropy', 'bar-plot', 'height')]
-        height2 = options[('Line Plot', 'Interaction Entropy', 'bar-plot', 'height')]
+        width1 = 100 - options[('Bar Plot', 'IE/C2 Entropy', 'bar-plot', 'width')]
+        width2 = options[('Bar Plot', 'IE/C2 Entropy', 'bar-plot', 'width')]
+        height1 = 100 - options[('Bar Plot', 'IE/C2 Entropy', 'bar-plot', 'height')]
+        height2 = options[('Bar Plot', 'IE/C2 Entropy', 'bar-plot', 'height')]
 
         if 'ie' in options:
             gs = gridspec.GridSpec(2, 2, height_ratios=[height1, height2], width_ratios=[width1, width2])
@@ -188,29 +188,35 @@ class LineChart(ChartsBase):
                                              color=rgb2rgbf(options[('Line Plot', 'line-color')]),
                                              linewidth=options[('Line Plot', 'line-width')], ax=axes)
             # plot the ie segment
-            ie_color = rgb2rgbf(options[('Line Plot', 'Interaction Entropy', 'ie-color')])
+            ie_color = rgb2rgbf(options[('Bar Plot', 'IE/C2 Entropy', 'ie-color')])
             sns.lineplot(data=data['iedata'], color=ie_color, ax=axes)
             ax2 = self.fig.add_subplot(gs[1, 1])
-            r_sigma = rgb2rgbf(options[('Line Plot', 'Interaction Entropy', 'sigma-color', 'reliable')])
-            nr_sigma = rgb2rgbf(options[('Line Plot', 'Interaction Entropy', 'sigma-color', 'non-reliable')])
+            r_sigma = rgb2rgbf(options[('Bar Plot', 'IE/C2 Entropy', 'sigma-color', 'reliable')])
+            nr_sigma = rgb2rgbf(options[('Bar Plot', 'IE/C2 Entropy', 'sigma-color', 'non-reliable')])
             colors = [ie_color, r_sigma if np.all(data.loc[:, ['sigma']].mean() < 3.6) else nr_sigma]
             self.ie_bar = sns.barplot(data=data[['iedata', 'sigma']], ax=ax2, palette=colors)
             numf = data['iedata'].count()
             self.ie_bar.set(xticklabels=[f"ie\n(last\n {numf} frames)", "σ(Int.\nEnergy)"])
             self.ie_barlabel = self.ie_bar.bar_label(self.ie_bar.containers[0],
-                                                     size=options[('Line Plot', 'Interaction Entropy', 'bar-plot',
+                                                     size=options[('Bar Plot', 'IE/C2 Entropy', 'bar-plot',
                                                                    'bar-label-fontsize')],
                                                      fmt='%.2f',
-                                                     padding=options[('Line Plot', 'Interaction Entropy', 'bar-plot',
+                                                     padding=options[('Bar Plot', 'IE/C2 Entropy', 'bar-plot',
                                                                       'bar-label-padding')])
             sns.despine(ax=self.ie_bar)
         else:
             axes = self.fig.subplots(1, 1)
             self.line_plot_ax = sns.lineplot(data=data, color=rgb2rgbf(options[('Line Plot', 'line-color')]),
                                              linewidth=options[('Line Plot', 'line-width')], label=data.name, ax=axes)
-            moving_avg = sns.lineplot(data=data.rolling(2, min_periods=1).mean(), color='red',
-                                      linewidth=options[('Line Plot', 'line-width')], label='Mov. Av.',
-                                      ls='--', ax=axes)
+            if options[('Line Plot', 'Rolling average', 'show')]:
+                min_periods = 1 if options[('Line Plot', 'Rolling average', 'first_obs')] == 'start' else None
+                window = options[('Line Plot', 'Rolling average', 'window')]
+                moving_avg = sns.lineplot(data=data.rolling(window, min_periods=min_periods).mean(),
+                                          color=rgb2rgbf(options[('Line Plot', 'Rolling average', 'color')]),
+                                          linewidth=options[('Line Plot', 'Rolling average', 'width')],
+                                          label='Mov. Av.',
+                                          ls=options[('Line Plot', 'Rolling average', 'style')],
+                                          ax=axes)
         self.cursor = Cursor(axes, useblit=True, color='black', linewidth=0.5, ls='--')
 
         self.setWindowTitle(options['subtitle'])
@@ -233,8 +239,8 @@ class LineChart(ChartsBase):
 
         if self.ie_bar:
             for label in self.ie_barlabel:
-                label.set_fontsize(options[('Line Plot', 'Interaction Entropy', 'bar-plot', 'bar-label-fontsize')])
-            self.ie_bar.tick_params(labelsize=options[('Line Plot', 'Interaction Entropy', 'bar-plot',
+                label.set_fontsize(options[('Bar Plot', 'IE/C2 Entropy', 'bar-plot', 'bar-label-fontsize')])
+            self.ie_bar.tick_params(labelsize=options[('Bar Plot', 'IE/C2 Entropy', 'bar-plot',
                                                        'axes-fontsize')])
         self.draw()
 
@@ -249,11 +255,12 @@ class BarChart(ChartsBase):
         self.fig.set_size_inches(options[('Bar Plot', 'figure', 'width')],
                                  options[('Bar Plot', 'figure', 'height')])
         self.bar_frames = False
+        palette = (sns.color_palette(options[('Bar Plot', 'palette')], n_colors=data.columns.size)
+                   if options[('Bar Plot', 'use-palette')] else None)
         if options['groups'] and options[('Bar Plot', 'subplot-components')]:
             self.axes = self.fig.subplots(1, len(options['groups']), sharey=True,
                                           gridspec_kw={'width_ratios': [len(x) for x in options['groups'].values()]})
-            palette = (sns.color_palette(options[('Bar Plot', 'palette')], n_colors=data.columns.size)
-                       if options[('Bar Plot', 'use-palette')] else None)
+
             s = 0
             for c, g in enumerate(options['groups']):
                 bar_plot_ax = sns.barplot(data=data[options['groups'][g]], ci="sd",
@@ -278,9 +285,9 @@ class BarChart(ChartsBase):
         else:
             self.axes = self.fig.subplots(1, 1)
             if 'c2' in options:
-                ie_color = rgb2rgbf(options[('Line Plot', 'Interaction Entropy', 'ie-color')])
-                r_sigma = rgb2rgbf(options[('Line Plot', 'Interaction Entropy', 'sigma-color', 'reliable')])
-                nr_sigma = rgb2rgbf(options[('Line Plot', 'Interaction Entropy', 'sigma-color', 'non-reliable')])
+                ie_color = rgb2rgbf(options[('Bar Plot', 'IE/C2 Entropy', 'ie-color')])
+                r_sigma = rgb2rgbf(options[('Bar Plot', 'IE/C2 Entropy', 'sigma-color', 'reliable')])
+                nr_sigma = rgb2rgbf(options[('Bar Plot', 'IE/C2 Entropy', 'sigma-color', 'non-reliable')])
                 palette = [ie_color, r_sigma if np.all(data.loc[:, ['sigma']].mean() < 3.6) else nr_sigma]
 
             bar_plot_ax = sns.barplot(data=data, ci="sd", errwidth=1, ax=self.axes, palette=palette,