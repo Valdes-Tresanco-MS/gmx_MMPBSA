052416adc3fb08fce921a4ea84fe86883d6c7bad
Mario <mariosergiovaldes145@gmail.com>
2022-02-26 12:55:54 -0500

    completion script updated

--- GMXMMPBSA/GMXMMPBSA.sh
+++ GMXMMPBSA/GMXMMPBSA.sh
@@ -3,7 +3,6 @@
 #                          GPLv3 LICENSE INFO
 #
 # Copyright (C) 2020  Mario S. Valdes-Tresanco and Mario E. Valdes-Tresanco
-# Copyright (C) 2014  Jason Swails, Bill Miller III, and Dwight McGee
 #
 #  Project: https://github.com/Valdes-Tresanco-MS/gmx_MMPBSA
 #
@@ -52,27 +51,29 @@ local p c
 COMPREPLY=() c=${COMP_WORDS[COMP_CWORD]} p=${COMP_WORDS[COMP_CWORD-1]}
 if (( $COMP_CWORD <= 1 )) || [[ $c == -* ]];
  then
-   COMPREPLY=( $(compgen  -W ' -h -v --input-file-help -O --overwrite -prefix -i -xvvfile -o -do -eo -deo -nogui -s
-    --stability -cs -ci -cg -ct -cp -cr -rs -ri -rg -rt -rp -lm -ls -li -lg -lt -lp  -n -q -h -make-mdins -use-mdins
-    -rewrite-output --clean --version --help' -- $c)); return 0; fi
+   COMPREPLY=( $(compgen  -W ' -h --help -v --version --input-file-help --create_input -O --overwrite -prefix -i
+   -xvvfile -o -do -eo -deo -nogui -s --stability -cs -ci -cg -ct -cp -cr -rs -ri -rg -rt -rp -lm -ls -li -lg -lt -lp
+   --rewrite-output --clean' -- $c)); return 0; fi
 case "$p" in
 
+--create_input)    COMPREPLY=( $(compgen -S ' ' -W $'gb\npb\nrism\nala\ndecomp\nnmode\nall' -- $c ));;
+-i)                COMPREPLY=( $(compgen -S ' ' -X '!*@(.in|)' -f -- $c ; compgen -S '/' -d $c));;
 -xvvfile)          COMPREPLY=( $(compgen -S ' ' -X '!*@(.xvv|)' -f -- $c ; compgen -S '/' -d $c));;
 -o)                COMPREPLY=( $(compgen -S ' ' -X '!*@(.dat|.txt|)' -f -- $c ; compgen -S '/' -d $c));;
 -do)               COMPREPLY=( $(compgen -S ' ' -X '!*@(.dat|.txt|)' -f -- $c ; compgen -S '/' -d $c));;
--eo)               COMPREPLY=( $(compgen -S ' ' -X '!*@(.csv||)' -f -- $c ; compgen -S '/' -d $c));;
+-eo)               COMPREPLY=( $(compgen -S ' ' -X '!*@(.csv|)' -f -- $c ; compgen -S '/' -d $c));;
 -deo)              COMPREPLY=( $(compgen -S ' ' -X '!*@(.csv|)' -f -- $c ; compgen -S '/' -d $c));;
--cs)               COMPREPLY=( $(compgen -S ' ' -X '!*@(.tpr|.pdb|.gro|)' -f -- $c ; compgen -S '/' -d $c));;
+-cs)               COMPREPLY=( $(compgen -S ' ' -X '!*@(.tpr|.pdb|)' -f -- $c ; compgen -S '/' -d $c));;
 -ci)               COMPREPLY=( $(compgen -S ' ' -X '!*@(.ndx|)' -f -- $c ; compgen -S '/' -d $c));;
 -ct)               COMPREPLY=( $(compgen -S ' ' -X '!*@(.xtc|.trr|.pdb|)' -f -- $c ; compgen -S '/' -d $c));;
 -cp)               COMPREPLY=( $(compgen -S ' ' -X '!*@(.top|)' -f -- $c ; compgen -S '/' -d $c));;
 -cr )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.pdb|)' -f -- $c ; compgen -S '/' -d $c));;
--rs )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.tpr|.pdb|.gro|)' -f -- $c ; compgen -S '/' -d $c));;
+-rs )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.tpr|.pdb|)' -f -- $c ; compgen -S '/' -d $c));;
 -ri )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.ndx|)' -f -- $c ; compgen -S '/' -d $c));;
 -rt )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.xtc|.trr|.pdb|)' -f -- $c ; compgen -S '/' -d $c));;
 -rp )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.top|)' -f -- $c ; compgen -S '/' -d $c));;
 -lm )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.mol2|)' -f -- $c ; compgen -S '/' -d $c));;
--ls )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.tpr|.pdb|.gro|)' -f -- $c ; compgen -S '/' -d $c));;
+-ls )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.tpr|.pdb|)' -f -- $c ; compgen -S '/' -d $c));;
 -li )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.ndx|)' -f -- $c ; compgen -S '/' -d $c));;
 -lt )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.xtc|.trr|.pdb|)' -f -- $c ; compgen -S '/' -d $c));;
 -lp )              COMPREPLY=( $(compgen -S ' ' -X '!*@(.top|)' -f -- $c ; compgen -S '/' -d $c));;
@@ -85,7 +86,7 @@ local p c
 COMPREPLY=() c=${COMP_WORDS[COMP_CWORD]} p=${COMP_WORDS[COMP_CWORD-1]}
 if (( $COMP_CWORD <= 1 )) || [[ $c == -* ]];
   then
-    COMPREPLY=( $(compgen  -W ' -h -v --version --help -f --file -r --recursive' -- $c));
+    COMPREPLY=( $(compgen  -W ' -h --help -v --version -f --files -r --recursive' -- $c));
     return 0;
 fi
 case "$p" in
@@ -100,12 +101,11 @@ local p c
 COMPREPLY=() c=${COMP_WORDS[COMP_CWORD]} p=${COMP_WORDS[COMP_CWORD-1]}
 if (( $COMP_CWORD <= 1 )) || [[ $c == -* ]];
   then
-    COMPREPLY=( $(compgen  -W ' -h -v --version --help -f --folder -t --test -n --num_processors' -- $c));
+    COMPREPLY=( $(compgen  -W ' -h --help -v --version -t -f --folder --test -r --reuse -ng --nogui -n
+    --num_processors' -- $c));
     return 0;
 fi
 case "$p" in
--t)       COMPREPLY=( $(compgen -S ' ' -W $'all\nminimal\nprot_lig_st\nprot_prot\nprot_dna\nmemb_prot\nprot_glycan
-            \nmetalloprot_pep\nprot_dna_rna_ions_lig\nprot_lig_charmm\nala_scan\nstability\ndecomp\nprot_lig_mt\nie
-            \nnmode\n3drism' --  $c));;
+-t)       COMPREPLY=( $(compgen -S ' ' -W $'0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18' --  $c));;
 esac }
 complete -F _gmxmmpbsa_test_compl gmx_MMPBSA_test
