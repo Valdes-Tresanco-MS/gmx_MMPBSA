d829b3f2a2bce69a7e68f9d9b26aed8b4af5b5c0
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2022-04-16 15:44:52 -0500

    fixed compatibility with versions <1.5.2 and 1.4.x

--- GMXMMPBSA/output_file.py
+++ GMXMMPBSA/output_file.py
@@ -320,7 +320,7 @@ def write_outputs(app):
                '\n3D-RISM:\n\n', '\n3D-RISM (Gauss. Fluct.):\n\n', '\n3D-RISM (PC+):\n\n')
     # Now print out the Free Energy results
     for i, key in enumerate(outkeys):
-        if not INPUT[triggers[i]]:
+        if triggers[i] not in INPUT or not INPUT[triggers[i]]:
             continue
         if not INPUT['mutant_only']:
             final_output.write(headers[i])
@@ -473,7 +473,7 @@ def write_decomp_output(app):
         dec_energies = writer(dec_energies_f)
 
     for i, key in enumerate(outkeys):
-        if not INPUT[triggers[i]]:
+        if triggers[i] not in INPUT or not INPUT[triggers[i]]:
             continue
         if not INPUT['mutant_only']:
             if stability:
